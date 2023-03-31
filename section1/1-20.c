270f79b212b5903987e2ddb67b1c62768125afc0
smllb <samuel.moreira6@fatec.sp.gov.br>
2023-03-31 10:49:23 -0300

    aa

--- section1/1-20.c
+++ section1/1-20.c
@@ -1,36 +1,46 @@
-s
+#include <stdio.h>
 
 int actualColumn = 0;
 
 void updateActualColumn () {
     actualColumn++;
-    if (actualColumn >7) {
+    if (actualColumn >8) {
         actualColumn = 0;
-    }   
+    }
+    // printf("actclmn %x ", actualColumn);
+    printf("actclmn %x ", actualColumn);
 }
 
+
+int retrieveNextSpot() {
+    int nextStop = 8-actualColumn;
+    return nextStop;
+
+}
+
+int updateColumnAndReturnNextTabSpot() {
+    updateActualColumn();
+    int nextTabSpot = retrieveNextSpot();
+    return nextTabSpot;
+}
 int main()
 {
     char c;  
     int nextTabStop;
     while(( c = getchar()) != EOF ) {
-        nextTabStop = 7-actualColumn;
-    //printf("nextabstop %d|", nextTabStop);
+        
         if (c == '\t') {
-            for (int i=0;i<nextTabStop;i++) {
-                //printf("%d", i);
-                updateActualColumn();
-                //printf("%d", actualColumn);
                 putchar(' ');
-            }    
-
+            
         } else if (c == '\n') {
             actualColumn = 0;
 
         } else {
            // printf("%d", actualColumn);
+
             putchar(c);
-            updateActualColumn();
+            nextTabStop = updateColumnAndReturnNextTabSpot();
+            
 
         }
         
